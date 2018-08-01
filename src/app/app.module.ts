173364e450d60692840674cad0b02e8f2d61d3e2
Emal <emalisuranga94@gmail.com>
2018-08-01 08:44:08 +0530

    angular update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+
+import { AuthService } from './service/auth.service';
+
+
+const routes: Routes = [
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/register/register.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+// import { Router } from '@angular/router';
+import { AuthService } from '../service/auth.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+
+  name: String;
+  username: String;
+  email: String;
+  password: String;
+
+  constructor(
+    private authService: AuthService
+  ) { }
+
+  ngOnInit() {
+  }
+
+  registerData() {
+    // console.log('register data emal');
+    const user = {
+      name: this.name,
+      userName: this.username,
+      email: this.email,
+      password: this.password
+    };
+
+   // console.log(user);
+   this.authService.registerUser(user);
+  }
+
+}
